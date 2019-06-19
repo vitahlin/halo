7ec52daf85fcf3ee0f04235fe4aae287c0931682
Vitah <linw1225@gmail.com>
2019-06-19 18:01:22 +0800

    Feat：初始化

---
+++ src/main/java/com/vitah/halo/HaloApplication.java
@@ -0,0 +1,14 @@
+package com.vitah.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vitah
+ */
+@SpringBootApplication
+public class HaloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HaloApplication.class, args);
+    }
+}
