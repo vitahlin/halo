4ab22e1046ce88711bfee81900ffa5dd98149948
Vitah <linw1225@gmail.com>
2019-06-25 15:41:55 +0800

    Feat：UserByAccount表相关

---
+++ src/main/java/com/vitah/halo/entity/UserByAccount.java
@@ -0,0 +1,39 @@
+package com.vitah.halo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author vitah
+ */
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class UserByAccount extends AbstractAuditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "app_id")
+    private Integer appId;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
