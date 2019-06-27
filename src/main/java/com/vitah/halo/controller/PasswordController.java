a29cb82d3f58f4ef1299c0e087e85addd39b252b
Vitah <linw1225@gmail.com>
2019-06-27 19:32:34 +0800

    Refactor：路由文件调整

--- src/main/java/com/vitah/halo/controller/PasswordController.java
+++ src/main/java/com/vitah/halo/controller/PasswordController.java
@@ -1,8 +1,11 @@
 package com.vitah.halo.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.vitah.halo.constant.CodeEnum;
+import com.vitah.halo.entity.UserByAccount;
+import com.vitah.halo.exception.BusinessException;
 import com.vitah.halo.repository.UserByAccountRepository;
-import com.vitah.halo.service.UserService;
+import com.vitah.halo.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +13,28 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author vitah
  */
-@RestController
-public class UserController {
-
-    @Autowired
-    private UserService userService;
+public class PasswordController {
 
     @Autowired
     private UserByAccountRepository userByAccountRepository;
 
     /**
-     * 新用户注册
+     * 重置密码
      *
+     * @param appId
+     * @param platform
+     * @param deviceId
+     * @param email
+     * @param password
+     * @param code
      * @return
      */
-    @RequestMapping(value = "/user/signup", method = RequestMethod.POST)
-    public ResponseEntity<Object> signUp(
+    @RequestMapping(value = "/user/password/reset", method = RequestMethod.PUT)
+    public ResponseEntity<Object> resetPassword(
         @RequestHeader(value = "X-APP-ID") Integer appId,
         @RequestHeader(value = "X-Platform") Integer platform,
         @RequestHeader(value = "X-Device-ID") String deviceId,
@@ -38,10 +42,15 @@ public class UserController {
         @RequestParam(value = "password") String password,
         @RequestParam(value = "code") String code
     ) {
-        String token = userService.newUser(appId, platform, email, password);
+        UserByAccount userByAccount = userByAccountRepository.findByEmail(email);
+        if (userByAccount == null) {
+            throw new BusinessException(CodeEnum.EMAIL_NOT_EXIST, HttpStatus.BAD_REQUEST);
+        }
 
-        JSONObject obj = new JSONObject();
-        obj.put("token", token);
-        return new ResponseEntity<>(obj, HttpStatus.OK);
+        String secretPassword = PasswordUtil.secret(password);
+        userByAccount.setPassword(secretPassword);
+        userByAccountRepository.save(userByAccount);
+
+        return new ResponseEntity<>(new JSONObject(), HttpStatus.OK);
     }
 }
