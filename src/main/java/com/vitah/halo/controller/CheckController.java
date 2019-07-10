fc6ee301d13abbf19ca88b6452e6196712c6cc64
Vitah <linw1225@gmail.com>
2019-07-10 15:40:56 +0800

    Refactor：token验证接口删除无用参数

--- src/main/java/com/vitah/halo/controller/CheckController.java
+++ src/main/java/com/vitah/halo/controller/CheckController.java
@@ -6,7 +6,6 @@ import com.vitah.halo.security.AuthenticationFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +20,7 @@ public class CheckController {
     private AuthenticationFacade authenticationFacade;
 
     @RequestMapping(value = "/auth/check", method = RequestMethod.POST)
-    public ResponseEntity<Object> resetPassword(
-        @RequestHeader(value = "X-APP-ID") Integer appId,
-        @RequestHeader(value = "X-Platform") Integer platform,
-        @RequestHeader(value = "X-Device-ID") String deviceId
-    ) {
+    public ResponseEntity<Object> resetPassword() {
         User user = authenticationFacade.currentUser();
         return new ResponseEntity<>(UserMapper.INSTANCE.userToDTO(user), HttpStatus.OK);
     }
